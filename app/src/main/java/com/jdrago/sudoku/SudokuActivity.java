8a82cd21ac9b0358678729b58f2dad9632a97e8c
Joe Drago <joedrago@gmail.com>
2017-09-05 01:11:06 -0700

    Initial commit (generated from wizard)

---
+++ app/src/main/java/com/jdrago/sudoku/SudokuActivity.java
@@ -0,0 +1,12 @@
+package com.jdrago.sudoku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SudokuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
